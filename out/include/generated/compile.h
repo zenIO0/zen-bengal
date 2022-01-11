3fd0bfa84e0082eece8c2e019f81b549cfaa67a6
Alexey <lexaxx14@gmail.com>
2022-01-11 17:10:02 +0000

    arm64: configs: ZenKernel

---
+++ out/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* |MoonBase|™ SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1 |MoonBase|™ SMP PREEMPT Tue Jan 11 16:49:43 UTC 2022"
+#define LINUX_COMPILE_BY "jsx"
+#define LINUX_COMPILE_HOST "igloo"
+#define LINUX_COMPILER "Proton clang version 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a), LLD 13.0.0 (https://github.com/llvm/llvm-project b4fd512c36ca344a3ff69350219e8b0a67e9472a)"
